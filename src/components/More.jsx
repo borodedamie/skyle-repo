327f61126e71000a8042870f740fc06e5082c6c1
Olatunji Tolulope Oluwafunmilayo <olatunjitolulope4@gmail.com>
2024-01-19 16:50:32 +0100

    reduced spacing

--- src/components/More.jsx
+++ src/components/More.jsx
@@ -23,40 +23,86 @@ const More = () => {
   const screenWidth = useScreenWidth();
 
   return (
-    <div className="bg-[#0C0C0C] border-[0.5px] z-[10] md:h-[420px] flex flex-col items-center justify-center bg-transparent mx-auto  text-[white] border-[white] bg-none rounded-[41px]  relative">
-        <p className="text-[#FFFFFF] text-center text-[30px] pt-[2rem] md:text-[40px] px-[2rem] md:px-[1rem] lg:px-0 leading-[31.8px] md:leading-[42.4px] font-[700] font-AtypDisplay">
-          Apps, Websites, Logos & More.
-        </p>
+    <div className="bg-[#0C0C0C] border-[0.5px] z-[10] md:h-[420px] bg-transparent   text-[white] flex justify-center mx-auto flex-col border-[white] bg-none rounded-[41px]  relative">
+      <p className="text-[#FFFFFF] text-center text-[30px] pt-[2rem] md:text-[40px] px-[2rem] md:px-[1rem] lg:px-0 leading-[31.8px] md:leading-[42.4px] font-[700] font-AtypDisplay">
+        Apps, Websites, Logos & More.
+      </p>
 
-        {screenWidth <= 767 ? (
-          <div className="text-left justify-center flex" >
+      {screenWidth <= 767 ? (
+        <div className="text-left justify-center flex">
           <div className="grid grid-cols-2 place-items-start text-left px-[1rem] py-[5rem] gap-x-[3rem] gap-y-10 justify-center leading-[16.96px] font-[400] text-[16px]">
             {moremobile.map((moremobiles, s) => {
               return (
-                <div className='text-left relative z-[10] capitalize' key={s}>
-                  <p className='w-[11rem] text-[16px] font-[Inter] leading-[16.96px] font-[400]'>{moremobiles.text}</p>
+                <div className="text-left relative z-[10] capitalize" key={s}>
+                  <p className="w-[11rem] text-[16px] font-[Inter] leading-[16.96px] font-[400]">
+                    {moremobiles.text}
+                  </p>
                 </div>
               );
             })}
           </div>
         </div>
-        ) : (
-          <div className="text-left justify-center flex">
-            <div className="grid grid-cols-2 place-items-start md:place-content-start md:grid-cols-5 text-left px-[1rem] md:px-[2rem] py-[5rem] md:py-[4rem] gap-x-[4rem] xl:gap-x-[6rem] gap-y-10 justify-center leading-[16.96px] font-[400] text-[16px]">
-              {more.map((moretexts, m) => {
-                return (
-                  <div
-                    className="font-[Inter] text-left relative z-[10] capitalize"
-                    key={m}
-                  >
-                    <p >{moretexts.text}</p>
-                  </div>
-                );
-              })}
+      ) : (
+          <div className="flex flex-row text-left md:py-[4rem] gap-y-10 justify-around">
+            <div>
+              <p className="font-[Inter] px-[1rem] py-[1rem]  text-left leading-[16.96px] font-[400] text-[16px] relative z-[10] capitalize">
+                Websites
+              </p>
+              <p className="font-[Inter] px-[1rem] py-[1rem]  text-left leading-[16.96px] font-[400] text-[16px] relative z-[10] capitalize">
+                Signage
+              </p>
+              <p className="font-[Inter] text-left px-[1rem] py-[1rem]  leading-[16.96px] font-[400] text-[16px] relative z-[10] capitalize">
+                Packaging
+              </p>
             </div>
+            <div>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Slide Decks
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Infographics
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Brand Guides
+              </p>
+            </div>
+            <div>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Motion Graphics
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Social Media Graphics
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Wireframes
+              </p>
+            </div>
+            <div>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Mobile Apps
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Brochures
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Pitch Decks
+              </p>
+            </div>
+            <div>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Email Graphics
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Blog Graphics
+              </p>
+              <p className="font-[Inter] text-left leading-[16.96px] px-[1rem] py-[1rem] font-[400] text-[16px] relative z-[10] capitalize">
+                Billboards
+              </p>
+            </div>
+           
           </div>
-        )}
-      </div>
+      )}
+    </div>
   );
 };
 
--- src/components/Whatwedo.jsx
+++ src/components/Whatwedo.jsx
@@ -3,8 +3,8 @@ import { whatwedo } from "../assets/utils/data";
 import creative from '../assets/gif/creative.gif'
 const Whatwedo = () => {
   return (
-    <div className="bg-[#0C0C0C] px-[20px] md:px-[80px]  flex flex-col md:flex-row justify-between items-start pt-[5rem] pb-[0rem] md:pt-[8rem] md:pb-[8rem] gap-[15vw] md:gap-[10vw] lg:gap-[20vw] max-w-[100%] w-[100%]">
-      <div className="flex flex-col justify-between gap-y-[7vw] items-start max-w-[100%] md:max-w-[33%]">
+    <div className="bg-[#0C0C0C] px-[20px] md:px-[80px]  flex flex-col md:flex-row justify-between items-start pt-[5rem] pb-[0rem] md:pt-[8rem] md:pb-[8rem] gap-[15vw] md:gap-[10vw] lg:gap-[15vw] max-w-[100%] w-[100%]">
+      <div className="flex flex-col justify-between gap-y-[2vw] items-start max-w-[100%] md:max-w-[33%]">
         <div className="border-b border-[#595959] pb-[1rem] md:pb-[2rem]">
           <h2 className="pb-[20px] font-[700] font-AtypDisplay text-[40px] leading-[38.6px] text-[white]">
             What We Do
